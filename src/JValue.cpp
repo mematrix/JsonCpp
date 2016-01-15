ae7fa5976ed1b59f77216a5eaa8ed3a4be707905
qife <839259646@qq.com>
2016-01-15 21:29:35 +0800

    complete value parse

--- src/JValue.cpp
+++ src/JValue.cpp
@@ -9,23 +9,35 @@ using namespace JsonCpp;
 const char *JValue::Parse(const char *str)
 {
     str = JsonUtil::SkipWhiteSpace(str);
-    if(*str == '\"'){
+    if (*str == '\"')
+    {
         // TODO: Parse string value
+        ++str;
+        autoMem.value.pStr = new std::string(JsonReader::ReadString(&str));
         return str;
     }
-    if(std::strncmp(str, "true", 4) == 0){
+    if (std::strncmp(str, "true", 4) == 0)
+    {
         autoMem.type = JValueType::boolean;
         autoMem.value.bVal = true;
-        return str+4;
+        return str + 4;
     }
-    if(std::strncmp(str, "false", 5) == 0){
-        autoMem.type=JValueType::boolean;
-        autoMem.value.bVal= false;
-        return str+5;
+    if (std::strncmp(str, "false", 5) == 0)
+    {
+        autoMem.type = JValueType::boolean;
+        autoMem.value.bVal = false;
+        return str + 5;
     }
-    if(std::strncmp(str, "null", 4) == 0){
-        autoMem.type= JValueType::null;
-        return str+4;
+    if (std::strncmp(str, "null", 4) == 0)
+    {
+        autoMem.type = JValueType::null;
+        return str + 4;
     }
-    // TODO
+
+    // TODO: Parse number value
+    char *end;
+    autoMem.value.num = std::strtod(str, &end);
+    JsonUtil::Assert(end != str);
+    autoMem.type = JValueType::number;
+    return end;
 }
