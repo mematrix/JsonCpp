dcd532362b9f0ef95f77dc349c3cdbb937c6ab0b
qife <839259646@qq.com>
2016-01-16 21:57:58 +0800

    add object realization

--- src/JObject.cpp
+++ src/JObject.cpp
@@ -44,3 +44,68 @@ const char *JObject::Parse(const char *str)
         return str + 1;
     }
 }
+
+JValueType JObject::GetType() const
+{
+    return JValueType::object;
+}
+
+bool JObject::operator bool() const
+{
+    // or throw exception
+    return !children.empty();
+}
+
+double JObject::operator double() const
+{
+    return children.size();
+}
+
+std::string JObject::operator std::string() const
+{
+    return ToString();
+}
+
+const JToken &JObject::operator[](const std::string &str) const
+{
+    return GetValue(str);
+}
+
+const JToken &JObject::operator[](long index) const
+{
+    return GetValue(index);
+}
+
+const JToken &JObject::GetValue(const std::string &str) const
+{
+    return *(children.at(str).get());
+}
+
+const JToken &JObject::GetValue(long) const
+{
+    throw JsonException("Access not support");
+}
+
+const std::string &JObject::ToString() const
+{
+    if (objString == nullptr)
+    {
+        objString = new std::string();
+        objString->push_back('{');
+        if (!children.empty())
+        {
+            for (const auto &child : children)
+            {
+                objString->push_back('\"');
+                objString->append(child.first);
+                objString->append("\":");
+                objString->append(child.second->ToString());
+                objString->push_back(',');
+            }
+            objString->pop_back();
+        }
+        objString->push_back('}');
+    }
+
+    return *objString;
+}
