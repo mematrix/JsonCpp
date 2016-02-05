04a3a3f73612b4631d23071cd2f24a8405f0cbdb
qife <839259646@qq.com>
2016-02-05 22:18:42 +0800

    add test of json path syntax

--- test/test.cpp
+++ test/test.cpp
@@ -17,6 +17,49 @@ int main(void)
 
         // output: {"aaa":11,"bbb":"test","ccc":true,"ddd":[23,34,45,12],"eee":{"er":null}}
         cout << obj.ToString() << endl;
+
+        auto token = obj.SelectToken("$.ddd[2]");
+        if (token)
+        {
+            // output: $.ddd[2]: 45
+            cout << "$.ddd[2]: " << token->ToString() << endl;
+        }
+        else
+        {
+            cout << "no token found!" << endl;
+        }
+
+        auto tokens = obj.SelectTokens("$.ddd[:-1]");
+        if (tokens.size() != 0)
+        {
+            // output: $.ddd[:-1]: 23 34 45
+            cout << "$.ddd[:-1]: ";
+            for (auto t : tokens)
+            {
+                cout << t->ToString() << " ";
+            }
+            cout << endl;
+        }
+        else
+        {
+            cout << "no tokens found!" << endl;
+        }
+
+        auto filterTokens = obj.SelectTokens("$.ddd[?(@.value < 30)]");
+        if (filterTokens.size() != 0)
+        {
+            // output: $.ddd[?(@.value < 30)]: 23 12
+            cout << "$.ddd[?(@.value < 30)]: ";
+            for (auto t:filterTokens)
+            {
+                cout << t->ToString() << " ";
+            }
+            cout << endl;
+        }
+        else
+        {
+            cout << "no tokens found by filter!" << endl;
+        }
     }
     catch (JsonException e)
     {
