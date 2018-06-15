a3e9acfb90c9cba7df61165467402b0f716acd2e
mx <mx@me.com>
2018-06-16 02:17:55 +0800

    update test code. add timer monitor.

--- test/test.cpp
+++ test/test.cpp
@@ -3,103 +3,206 @@
 //
 
 #include <iostream>
+#include <fstream>
+#include <chrono>
 
 #include "JSONConvert.hpp"
+#include "JSONQuery.hpp"
 
 using namespace std;
 using namespace json;
 
-
-struct test
+struct es_search_result
 {
-    struct inner {
-        std::unique_ptr<std::string> er;
-        std::shared_ptr<std::string> msg;
+    struct shared
+    {
+        int64_t failed = 0;
+        int64_t skipped = 0;
+        int64_t successful = 0;
+        int64_t total = 0;
     };
 
-    int aaa;
-    std::string bbb;
-    bool ccc;
-    int ddd[6];
-    inner eee;
+    struct beat_info
+    {
+        string hostname;
+        string version;
+        string name;
+    };
+
+    struct metricset_info
+    {
+        string module;
+        string name;
+        string rtt;
+    };
+
+    struct fs_used
+    {
+        int64_t bytes = 0;
+        double pct = 0;
+    };
+
+    struct file_system_info
+    {
+        int free_files = 0;
+        string type;
+        fs_used used;
+        string device_name;
+        string mount_point;
+        int64_t total = 0;
+        int64_t files = 0;
+        int64_t free = 0;
+        int64_t available = 0;
+    };
+
+    struct system_info
+    {
+        file_system_info filesystem;
+    };
+
+    struct source
+    {
+        beat_info beat;
+        metricset_info metricset;
+        system_info system;
+    };
+
+    struct hits_info
+    {
+        string _id;
+        string _index;
+        double _score = 0;
+        source _source;
+        string _type;
+    };
+
+    struct hits_overview
+    {
+        std::list<hits_info> hits;
+        double max_score = 0;
+        int64_t total = 0;
+    };
+
+    hits_overview hits;
+    bool time_out = false;
+    int64_t took = 0;
+    shared _shared;
 };
 
-#define DESERIALIZE_A DESERIALIZE(aaa)
-#define DESERIALIZE_B DESERIALIZE(bbb)
-
 namespace json {
-DESERIALIZE_CLASS(test::inner, DESERIALIZE(er), DESERIALIZE(msg));
+DESERIALIZE_CLASS(es_search_result::shared, DESERIALIZE(failed), DESERIALIZE(skipped), DESERIALIZE(successful), DESERIALIZE(total));
 
-DESERIALIZE_CLASS(test, DESERIALIZE_A, DESERIALIZE_B, DESERIALIZE(ccc), DESERIALIZE(ddd), DESERIALIZE(eee));
+DESERIALIZE_CLASS(es_search_result::beat_info, DESERIALIZE(hostname), DESERIALIZE(version), DESERIALIZE(name));
+
+DESERIALIZE_CLASS(es_search_result::metricset_info, DESERIALIZE(module), DESERIALIZE(name), DESERIALIZE(rtt));
+
+DESERIALIZE_CLASS(es_search_result::fs_used, DESERIALIZE(bytes), DESERIALIZE(pct));
+
+DESERIALIZE_CLASS(es_search_result::file_system_info, DESERIALIZE(free_files), DESERIALIZE(type), DESERIALIZE(used), DESERIALIZE(device_name),
+                  DESERIALIZE(mount_point), DESERIALIZE(total), DESERIALIZE(files), DESERIALIZE(free), DESERIALIZE(available));
+
+DESERIALIZE_CLASS(es_search_result::system_info, DESERIALIZE(filesystem));
+
+DESERIALIZE_CLASS(es_search_result::source, DESERIALIZE(beat), DESERIALIZE(metricset), DESERIALIZE(system));
+
+DESERIALIZE_CLASS(es_search_result::hits_info, DESERIALIZE(_id), DESERIALIZE(_index), DESERIALIZE(_score), DESERIALIZE(_source), DESERIALIZE(_type));
+
+DESERIALIZE_CLASS(es_search_result::hits_overview, DESERIALIZE(hits), DESERIALIZE(max_score), DESERIALIZE(total));
+
+DESERIALIZE_CLASS(es_search_result, DESERIALIZE(hits), DESERIALIZE(time_out), DESERIALIZE(took), DESERIALIZE(_shared));
 }
 
-
-int main()
+class count_timer
 {
-    const char *json_str = "{\"aaa\":11,\"bbb\":\"test\",\"ccc\":true,\"ddd\":[23,34,45,  12],\"eee\":{ \"er\":null , \"msg\": \"error message. eee\"}}";
+    using tp = chrono::system_clock::time_point;
+    tp start_time;
+    tp end_time;
+
+public:
+    count_timer() = default;
+
+    void start()
+    {
+        start_time = chrono::system_clock::now();
+    }
+
+    void stop()
+    {
+        end_time = chrono::system_clock::now();
+    }
+
+    void print(const std::string &str)
+    {
+        auto time_ns = (end_time - start_time).count();
+        cout << "(" << str << ") time: " << time_ns << "ns = " << time_ns / 1000.0 << "us = " << time_ns / (1000.0 * 1000) << "ms" << endl;
+    }
+};
+
+
+int main(int argc, char **argv)
+{
+    if (argc != 2) {
+        cerr << "usage: " << argv[0] << " json_file_path" << endl;
+        return -1;
+    }
+
+    count_timer ct;
+    ifstream test_file(argv[1]);
+    if (!test_file) {
+        cerr << "can not open file: " << argv[1] << endl;
+        return -1;
+    }
+    std::string content((std::istreambuf_iterator<char>(test_file)), std::istreambuf_iterator<char>());
     int error_code = 0;
-    auto token = parse(json_str, &error_code);
+
+    ct.start();
+    auto token = parse(content, &error_code);
+    ct.stop();
+    ct.print("parse");
+
     if (token) {
-        cout << "parse success." << std::endl;
-        cout << "non-formatted json: " << to_string(*token) << std::endl;
-        cout << "formatted json: " << std::endl;
-        cout << to_string(*token, json_format_option::indent_tab) << std::endl;
         cout << std::endl;
-        cout << "deserialize..." <<std::endl;
-        test t;
-        deserialize(t, *token);
-        cout << "aaa: " << t.aaa << ", bbb: " << t.bbb << ", ccc: " << boolalpha << t.ccc << ", ddd: [";
-        for (int i = 0; i < 5; ++i) {
-            cout << t.ddd[i] << ", ";
-        }
-        cout << t.ddd[5] << "]";
-        cout << ", eee: {er: " << (t.eee.er == nullptr ? "null" : *(t.eee.er)) << ", msg: " << (t.eee.msg == nullptr ? "null" : *(t.eee.msg)) << "}" << std::endl;
+        ct.start();
+        auto non_format_json = to_string(*token);
+        ct.stop();
+        ct.print("to_string");
+        // cout << "non-formatted json: " << non_format_json << std::endl;
+
+        cout << std::endl;
+        ct.start();
+        auto format_json = to_string(*token, json_format_option::indent_tab);
+        ct.stop();
+        ct.print("to_string format");
+        // cout << "formatted json: " << std::endl << format_json << std::endl;
+
+        cout << std::endl;
+        es_search_result result;
+        ct.start();
+        deserialize(result, *token);
+        ct.stop();
+        ct.print("deserialize");
+        cout << "took: " << result.took << "; hits item count: " << result.hits.hits.size() << endl;
+
+        cout << std::endl;
+        const char *dot_json_path = "$.hits.hits[*]._source.system.filesystem.used.bytes";
+        ct.start();
+        auto query_result = select_tokens(*token, dot_json_path);
+        ct.stop();
+        ct.print("dot_json_path");
+        cout << "query result count: " << query_result.size() << endl;
+
+        cout << std::endl;
+        const char *bracket_json_path = "$['hits']['hits'][*]['_source']['system']['filesystem']['used']['bytes']";
+        ct.start();
+        auto b_query_result = select_tokens(*token, bracket_json_path);
+        ct.stop();
+        ct.print("bracket_json_path");
+        cout << "query result count: " << b_query_result.size() << endl;
     } else {
         cerr << "parse failed. error code: " << error_code << ", msg: " << get_error_info(error_code) << std::endl;
     }
 
-    /* try {
-        json_object obj();
-
-        // output: {"aaa":11,"bbb":"test","ccc":true,"ddd":[23,34,45,12],"eee":{"er":null}}
-        cout << obj.ToString() << endl;
-
-        auto token = obj.SelectToken("$.ddd[2]");
-        if (token) {
-            // output: $.ddd[2]: 45
-            cout << "$.ddd[2]: " << token->ToString() << endl;
-        } else {
-            cout << "no token found!" << endl;
-        }
-
-        auto tokens = obj.SelectTokens("$.ddd[:-1]");
-        if (tokens.size() != 0) {
-            // output: $.ddd[:-1]: 23 34 45
-            cout << "$.ddd[:-1]: ";
-            for (auto t : tokens) {
-                cout << t->ToString() << " ";
-            }
-            cout << endl;
-        } else {
-            cout << "no tokens found!" << endl;
-        }
-
-        auto filterTokens = obj.SelectTokens("$.ddd[?(@.value < 30)]");
-        if (filterTokens.size() != 0) {
-            // output: $.ddd[?(@.value < 30)]: 23 12
-            cout << "$.ddd[?(@.value < 30)]: ";
-            for (auto t:filterTokens) {
-                cout << t->to_string() << " ";
-            }
-            cout << endl;
-        } else {
-            cout << "no tokens found by filter!" << endl;
-        }
-    }
-    catch (JsonException e) {
-        cout << e.message << endl;
-        return -1;
-    } */
+    // cin.get();
 
     return 0;
 }
